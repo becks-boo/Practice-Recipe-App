a5147e13a12d48bf4d4df91b87a4f4b8cec114c4
Igor Stein <igor_stein@yahoo.de>
2021-02-03 07:07:51 +0100

    create pages controller and root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "recipes#index"
-  resources :recipes, except: :index
+  root to: "pages#home"
+  resources :recipes
 end
