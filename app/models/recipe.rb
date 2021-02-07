ae3d5839c29ff072249efa5fb259259f54107f62
Igor Stein <igor_stein@yahoo.de>
2021-02-07 19:04:29 +0100

    create validations for ingredient, quantity and recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
   has_many :quantities
   has_many :ingredients, through: :quantities
+
+  validates :name, :description, :difficulty, presence: true
+  validates :difficulty, inclusion: { in: ["very easy", "easy", "moderate", "expert") }
+  validates :description, length: { minimum: 150 }
 end
