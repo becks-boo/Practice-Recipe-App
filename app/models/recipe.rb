af7f649df1a88f5d6bc40d86c144a139dcaad040
Igor Stein <igor_stein@yahoo.de>
2021-02-12 14:40:27 +0100

    import and organize css

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,6 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :quantities
 
   validates :name, :description, :difficulty, presence: true
-  validates :difficulty, inclusion: { in: ["very easy", "easy", "moderate", "expert") }
+  validates :difficulty, inclusion: { in: ["very easy", "easy", "moderate", "expert"] }
   validates :description, length: { minimum: 150 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
   resources :recipes
+  # Do all recipes routes individually
+  #      Prefix Verb   URI Pattern                 Controller#Action
+  #     recipes GET    /recipes(.:format)          recipes#index
+  #             POST   /recipes(.:format)          recipes#create
+  #  new_recipe GET    /recipes/new(.:format)      recipes#new
+  # edit_recipe GET    /recipes/:id/edit(.:format) recipes#edit
+  #      recipe GET    /recipes/:id(.:format)      recipes#show
+  #             PATCH  /recipes/:id(.:format)      recipes#update
+  #             PUT    /recipes/:id(.:format)      recipes#update
+  #             DELETE /recipes/:id(.:format)      recipes#destroy
 end
