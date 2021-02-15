8c0af2080b8f3c39ac667546d0e98b86a9c2d9a1
Igor Stein <igor_stein@yahoo.de>
2021-02-15 13:32:50 +0100

    update associations to favorite_recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ApplicationRecord
 
   has_many :quantities, dependent: :destroy
   has_many :ingredients, through: :quantities
+  has_many :favorite_recipes
 
   validates :name, :description, :difficulty, presence: true
   validates :difficulty, inclusion: { in: ["very easy", "easy", "moderate", "expert"] }
