46aaa5a63e27aba610cc3e7e197cf49b701e096a
Igor Stein <igor_stein@yahoo.de>
2021-02-15 12:34:58 +0100

    generate favorite_recipe model and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_195532) do
+ActiveRecord::Schema.define(version: 2021_02_15_113247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorite_recipes", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_favorite_recipes_on_recipe_id"
+    t.index ["user_id"], name: "index_favorite_recipes_on_user_id"
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -53,6 +62,8 @@ ActiveRecord::Schema.define(version: 2021_02_07_195532) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorite_recipes", "recipes"
+  add_foreign_key "favorite_recipes", "users"
   add_foreign_key "quantities", "ingredients"
   add_foreign_key "quantities", "recipes"
   add_foreign_key "recipes", "users"
